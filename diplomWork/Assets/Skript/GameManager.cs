23c5aa71170a59a9e85f9ec4de363f263d76ffec
ilyas <ilya.sabodan@mail.ru>
2024-06-12 18:36:56 +0300

    2 версия
    добавлены и установлены префабы рук без анимации. Добавлена таблица счета и сохранение очков

--- diplomWork/Assets/Skript/GameManager.cs
+++ diplomWork/Assets/Skript/GameManager.cs
@@ -6,17 +6,51 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // ключ по которому ищется значение  
+    private int key;
     public static int point;
     public static bool gameOn;
     [SerializeField] TextMeshProUGUI textpoint;
-    public void Update()
+    public List<TextMeshProUGUI> textpointsAttempt = new List<TextMeshProUGUI>();
+
+    private void Start()
     {
+        point = 0;
+        //PlayerPrefs.DeleteAll();
+        key = PlayerPrefs.GetInt("key");
+
+        for (int i = 0; i < 8; i++)
+        {
+            textpointsAttempt[i].text = textpointsAttempt[i].text + PlayerPrefs.GetInt($"point{i}");
+        }
+
+    }
+    public void Update()
+    {       
         textpoint.text= "point:" + point.ToString();       
     }
 
-    public static void Restart()
+    public  void Restart()
     {
+        // класс SceneManager отвечает за работу всех сцен в проекте, метод LoadScene загружает сцену по индексу.
+        PlayerPrefs.SetInt($"point{key}", point);
+
+        if (key > 7)
+        {
+            key = 0;
+            PlayerPrefs.SetInt($"point{key}", point);
+        }
+
+        key++;
+
+        PlayerPrefs.SetInt("key", key);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    // сохранение данных 
+
+    public void DeleateData()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
 }
--- diplomWork/Assets/Skript/moveBoll.cs
+++ diplomWork/Assets/Skript/moveBoll.cs
@@ -20,12 +20,19 @@ public class moveBoll : MonoBehaviour
                 rb.AddForce(vector3 * speed, ForceMode.Impulse);
         }
 
+        // Метод срабатывает при коллизии с другим объектом, у которого есть коллайдер и Rigidbody 
         private void OnCollisionEnter(Collision collision)
         {
+            // при коллизии с объектом с тегом "floor" мы добавляем скольжение к шару и передаем значение статической  переменной gameOn
             if (collision.gameObject.tag == "floor")
             {
                 addSpeed = true;
                 GameManager.gameOn = true;
+            }
+        // при коллизии с объектом с тегом "wall" мы выключаем скольжение шара
+            if (collision.gameObject.tag == "wall")
+            {
+                 addSpeed = false;
             }
         }
 }
